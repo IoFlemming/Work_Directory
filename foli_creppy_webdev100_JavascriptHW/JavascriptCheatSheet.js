f07f07370ae72757f9bfc3fd4a958d2cd4aaa9dd
Foli Creppy <folicreppy1@gmail.com>
2016-04-30 00:52:00 -0400

    First HW Commits

---
+++ foli_creppy_webdev100_JavascriptHW/JavascriptCheatSheet.js
@@ -0,0 +1,90 @@
+/*Variable*/
+var price1 = 5;
+var price2 = 6;
+console.log(price1 + price2);
+
+/*Alert*/
+window.alert("Warning! You Have A Virus on Your Macbook! Restart Macbook now!");
+
+/*String*/
+var statement = "I am the son of the one called \"Ragna\".";
+console.log(statement);
+
+/*Number*/
+var x = 5 + 7 + 9;
+console.log(x);
+
+/*Boolean*/
+var x = 20 > 18;
+console.log(x);
+
+/*Undefined*/
+var name;
+if (x = undefined) {
+txt = "x is undefined";
+}
+else {
+txt = "x is defined";
+}
+console.log(txt);
+
+/*Array*/
+var name = ["Banagher Links", "Gene Starwind", "Heero Yu"];
+window.alert();
+
+//3:
+
+var num_one = 1,
+	num_two = 2,
+	sum = num_one + num_two;
+
+console.log(sum);
+
+
+//4:
+
+var friends = [{
+	name: 'Bob',
+	age: 24,
+	gender: 'male'
+}, {
+	name: 'Jane',
+	age: 20,
+	gender: 'female'
+}, {
+	name: 'John',
+	age: 34,
+	gender: 'male'
+}];
+
+console.log(friends[0].name);
+console.log(friends[1].name);
+
+
+//5:
+
+// Trick question, just because variable isn't less than 10 doesn't mean it is greater,
+// it could be equal to 10 so just regular else wouldn't do
+if(x_value < 10) {
+	alert('Variable less than 10');
+} else if(x_value > 10) { {
+	console.log(x_value + ' is greater than 10');
+}
+
+//6:
+
+//this one isn't really a answer to write, you would basically have to change the value of that variable like:
+
+	var x_value = 5;
+
+//then run the code:
+
+	if(x_value < 10) {
+	window.alert('Variable less than 10');
+  else if(x_value > 10){
+    
+  }
+	console.log(x_value + 'is greater than 10');
+
+
+//and keep doing that every time u change the value...
